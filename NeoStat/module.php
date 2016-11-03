fdf4aca1faa83f63aca1c75fe22726f98fe54745
ccie32305 <philipp@neidlein.net>
2016-11-03 20:10:05 +0100

    Update module.php

--- NeoStat/module.php
+++ NeoStat/module.php
@@ -10,7 +10,7 @@ class NeoStat extends IPSModule
 		//These lines are parsed on Symcon Startup or Instance creation
         //You cannot use variables here. Just static values.
 		
-   		 $this->RegisterPropertyString("Name", "");
+   		 $this->RegisterPropertyString("NeoStatName", "");
 		 $this->RegisterVariableFloat("current_temperature", "Current Temperature");
 		 $this->RegisterVariableFloat("current_set_temperature", "Current Set Temperature");
 		 $this->RegisterVariableBoolean("heating", "Heating");
