c18c65d11501d8de8fbb2cafbdb9d03fbdfb3b9c
ccie32305 <philipp@neidlein.net>
2016-11-03 21:44:30 +0100

    Update module.php

--- NeoStat/module.php
+++ NeoStat/module.php
@@ -10,7 +10,7 @@ class NeoStat extends IPSModule
 	
 		//These lines are parsed on Symcon Startup or Instance creation
         //You cannot use variables here. Just static values.
-		/* $this->ConnectParent("{434a3a30-77cc-4a17-ab15-0d1f757d6bbf}"); */
+		$this->ConnectParent("{434a3a30-77cc-4a17-ab15-0d1f757d6bbf}"); 
 	   	 $this->RegisterPropertyString("NeoStatName","");
 		 $this->RegisterVariableFloat("current_temperature", "Current Temperature");
 		 $this->RegisterVariableFloat("current_set_temperature", "Current Set Temperature");
