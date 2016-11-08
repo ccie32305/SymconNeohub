12f9915f4201c58e63955a5ec06fd79256bce515
ccie32305 <philipp@neidlein.net>
2016-11-08 20:49:16 +0100

    Update module.php

--- Neohub/module.php
+++ Neohub/module.php
@@ -200,6 +200,7 @@ class Neohub extends IPSModule
 		IPS_LogMessage("1:",$SetTempID);
 		IPS_LogMessage("2:",$SetTemp);
 		IPS_LogMessage("3:",$SetTempDevice);
+		IPS_LogMessage("4:",IPS_GetProperty($SetTempID,"NeoStatName"));
 		/*
 	
 		$NeohubIP = $this->ReadPropertyString('NeohubIP');
