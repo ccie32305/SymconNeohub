b94b2b28a8d775c442f54b8332399da0322a900e
ccie32305 <philipp@neidlein.net>
2016-11-22 21:20:47 +0100

    Update module.php

--- NeoStat/module.php
+++ NeoStat/module.php
@@ -36,7 +36,7 @@ class NeoStat extends IPSModule
 	public function SetTemp(integer $setTemp)
 	{
 		IPS_LogMessage("NeoStatTemp-1:",$this->getNeoHubInstanceId());
-		IPS_LogMessage("NeoStatTemp-2:",$setTemp);
+		IPS_LogMessage("NeoStatTemp-2:",$this);
 		IPS_LogMessage("NeoStatTemp-3:",IPS_GetParent($this));
 		IPS_LogMessage("NeoStatTemp-4:",IPS_GetInstance(IPS_GetParent($this))['ConnectionID']);
 		IPS_LogMessage("NeoStatTemp-5:",$this);
